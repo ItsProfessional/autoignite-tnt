db4adc0105049c7747c2f8d16955623e6988a244
ItsProfessional <63961221+ItsProfessional@users.noreply.github.com>
2023-10-12 19:53:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Autoignite TNT
---
+++ src/main/java/me/proferk/autoignitetnt/mixin/TntBlockMixin.java
@@ -0,0 +1,17 @@
+package me.proferk.autoignitetnt.mixin;
+
+import net.minecraft.block.TntBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TntBlock.class)
+public abstract class TntBlockMixin {
+    @Redirect(method = "onBlockAdded", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isReceivingRedstonePower(Lnet/minecraft/util/math/BlockPos;)Z"))
+    private boolean autoignite(World instance, BlockPos blockPos)
+    {
+        return true;
+    }
+}
